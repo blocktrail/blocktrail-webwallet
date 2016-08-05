25a6c246b1c4bbacd8d0a7060b54fceec801df55
Ruben de Vries <ruben@rubensayshi.com>
2016-08-05 17:53:19 +0200

    hide switching translations until they're completely done

--- src/js/controllers/SettingsControllers.js
+++ src/js/controllers/SettingsControllers.js
@@ -4,6 +4,12 @@ angular.module('blocktrail.wallet')
                                          CONFIG, $modal) {
         $rootScope.pageTitle = 'SETTINGS';
 
+        $scope.translationsEnabled = false;
+
+        $scope.$on('translationsEnabled', function(n, o) {
+            console.log('translationsEnabled', n, v);
+        });
+
         $scope.profilePic = {
             newProfileImage: null,
             croppedProfileImage: null,
