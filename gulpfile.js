e54a945da28c716ad6feb8423e949dd648bab605
Ruben de Vries <ruben@rubensayshi.com>
2017-04-18 14:28:28 +0200

    use in-memory pouchdb when indexeddb is failing. delay angular.bootstrap until we've determined if indexeddb is supported.

--- gulpfile.js
+++ gulpfile.js
@@ -265,6 +265,7 @@ gulp.task('js:libs', ['appconfig'], function() {
             "./src/lib/angular-toggle-switch/angular-toggle-switch.js",
 
             "./src/lib/pouchdb/dist/pouchdb.js",
+            "./src/lib/pouchdb/dist/pouchdb.memory.js",
 
             "./src/lib/bowser/src/bowser.js",
 
