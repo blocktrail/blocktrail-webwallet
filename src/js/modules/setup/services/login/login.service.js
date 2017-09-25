0993d3ae42cb54f386d017054adf62303ad0a17c
Roman <panchenkors@gmail.com>
2017-09-25 14:40:42 +0200

    Update login.service.js

--- src/js/modules/setup/services/login/login.service.js
+++ src/js/modules/setup/services/login/login.service.js
@@ -128,7 +128,6 @@
     LoginFormService.prototype._errorHandler = function(response) {
         var error = {
             type: "MSG_BAD_NETWORK",
-            silentException: true,
             data: null
         };
 
