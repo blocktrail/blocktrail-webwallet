e30296bb828521a75bc666ce150f7a0eddfda4d9
Ruben de Vries <ruben@rubensayshi.com>
2017-12-15 15:16:34 +0100

    fix redirect spam

--- src/js/modules/setup/controllers/lost-lock/lost-lock.ctrl.js
+++ src/js/modules/setup/controllers/lost-lock/lost-lock.ctrl.js
@@ -5,10 +5,13 @@
         .controller("LostLockCtrl", LostLockCtrl);
 
 
-    function LostLockCtrl($scope, $stateParams, $window) {
+    function LostLockCtrl($scope, $stateParams, $timeout, $window) {
         if ($stateParams.refresh === "1") {
             $window.location.replace("/#/lostlock?refresh=0");
-            $window.location.reload();
+            // short timeout to avoid redirect loop
+            $timeout(function() {
+                $window.location.reload();
+            }, 100);
         }
     }
 
