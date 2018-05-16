6314fe021f0dbf1ae9f5e66dcf1ed3f03cbc5f80
Tobias Hoffmann <lacksfish@gmail.com>
2018-05-16 11:25:49 +0200

    refactor out to bitcoinLinkService

--- src/js/modules/wallet/controllers/payment-uri/payment-uri.ctrl.js
+++ src/js/modules/wallet/controllers/payment-uri/payment-uri.ctrl.js
@@ -5,29 +5,14 @@
         .controller("PaymentURICtrl", PaymentURIController);
     function PaymentURIController($state, $stateParams, bitcoinLinkService) {
         var scheme = $stateParams.scheme;
-        console.log(scheme);
-        //parse result for address
-        var elm = angular.element('<a>').attr('href', scheme )[0];
-
-        if (elm.protocol === 'bitcoin:' || elm.protocol === 'bitcoincash:') {
-            var uri = bitcoinLinkService.decodeBitcoin(scheme);
-            if (uri.options.r) {
+        return bitcoinLinkService.parse(scheme).then(function (sendInput) {
+            if(sendInput && (sendInput.network === 'bitcoin' || sendInput.network === 'bitcoincash')) {
                 $state.go('app.wallet.send', {
-                    protocol: elm.protocol.slice(0, -1),
-                    uri: scheme,
-                    paymentUrl: uri.options.r
-                });
-            } else if (uri.options.amount || uri.address) {
-                $state.go('app.wallet.send', {
-                    protocol: elm.protocol.slice(0, -1),
-                    uri: scheme,
-                    amount: uri.options.amount,
-                    address: uri.address
+                    sendInput: sendInput
                 });
             } else {
-                // something was horribly wrong?
                 $state.go('app.wallet.summary');
             }
-        }
+        });
     }
 })();
